ff9f6f0e596304d445afbc60e87ba15b22c340e9
jay <qkr2435@everon.co.kr>
2022-07-31 22:24:00 +0900

    gradle commit
    thymleaf gradle

---
+++ src/test/java/mvc2/thymeleafbasic/ThymeleafBasicApplicationTests.java
@@ -0,0 +1,15 @@
+package mvc2.thymeleafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+//test
+@SpringBootTest
+class ThymeleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
